784a98f4e4ebfeee935c130f55bb2164d4e25a10
HongfeiChenCU <hc3222@columbia.edu>
2021-04-15 15:49:17 -0700

    print for arrayaccess added

--- tests/test-array1.jl
+++ tests/test-array1.jl
@@ -2,14 +2,12 @@ int main() {
     string[] strArr;
     int[] intArr;
     int i;
-    string j;
     intArr = [1,2,3];
     i = intArr[2];
     print(i);
     strArr = ["h", "e", "y"];
     for (i = 0 ; i < 3 ; i = i + 1) {
-        j = strArr[i];
-        print(j);
+        print(strArr[i]);
     }
   return 0;
 }
